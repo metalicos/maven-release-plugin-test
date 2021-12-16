8ec7468b738a470e51b5d9745a81340f5abee62e
Ostap Komplikevych <ostap_komplikevych@epam.com>
2021-12-16 15:47:17 +0200

    added all files init

---
+++ src/main/java/com/example/mavenreleaseplugintest/MavenReleasePluginTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenreleaseplugintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenReleasePluginTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenReleasePluginTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mavenreleaseplugintest/MavenReleasePluginTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenreleaseplugintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenReleasePluginTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
